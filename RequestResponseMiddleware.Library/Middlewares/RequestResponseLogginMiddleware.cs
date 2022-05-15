f47b0f9846a2a6e22c5e60acfbd77bef1fde2575
hstbi <hstbi@HST>
2022-05-15 13:27:19 +0300

    Add project files.

---
+++ RequestResponseMiddleware.Library/Middlewares/RequestResponseLogginMiddleware.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RequestResponseMiddleware.Library.Middlewares
+{
+    public class RequestResponseLogginMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            await next(context);
+        }
+
+
+
+    }
+}
